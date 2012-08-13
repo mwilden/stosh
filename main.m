7417f9816cda540b91250a3756a60a242cfa7bf6
Mark Wilden <mark@mwilden.com>
2012-08-12 18:20:52 -0700

    main.mm -> main.m

--- main.m
+++ main.m
@@ -1,7 +1,3 @@
-#import <UIKit/UIKit.h>
-
-#import "GlaurungAppDelegate.h"
-
 int main(int argc, char *argv[]) {
   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
   int retVal = UIApplicationMain(argc, argv, nil, @"GlaurungAppDelegate");
