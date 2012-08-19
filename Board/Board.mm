cde87217213d40e9e3d48644752c4173853100e0
Mark Wilden <mark@mwilden.com>
2012-08-18 18:15:01 -0700

    Simply return the colors and don't worry about how they're created

--- Board/Board.mm
+++ Board/Board.mm
@@ -1,21 +1,17 @@
 #import "Board.h"
 
-static UIColor *darkSquareColor = [[UIColor colorWithRed: 0.20 green: 0.40 blue: 0.70 alpha: 1.0] retain];
-static UIColor *lightSquareColor = [[UIColor colorWithRed: 0.69 green: 0.78 blue: 1.0 alpha: 1.0] retain];
-static UIColor *highlightColor = [[UIColor purpleColor] retain];
-
 @implementation Board
 
 + (UIColor*)darkSquareColor {
-    return darkSquareColor;
+    return [UIColor colorWithRed: 0.20 green: 0.40 blue: 0.70 alpha: 1.0];
 }
 
 + (UIColor*)lightSquareColor {
-    return lightSquareColor;
+    return [UIColor colorWithRed: 0.69 green: 0.78 blue: 1.0 alpha: 1.0];
 }
 
 + (UIColor*)highlightColor {
-    return highlightColor;
+    return [UIColor purpleColor];
 }
 
 @end
